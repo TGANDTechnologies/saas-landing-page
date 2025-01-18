7a535ce9a07301578a1be9b8d0e886e45801731b
yash <yashvikram8250@gmail.com>
2025-01-18 23:55:46 +0530

    Adding Logoticker and ProductShowcase sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
-
+import { ProductShowcase } from "@/sections/ProductShowcase";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <LogoTicker />
-
+      <ProductShowcase/>
     </>
   );
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,26 +1,38 @@
 import acmeLogo from "@/assets/logo-acme.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
-import celestialLogo from '@/assets/logo-celestial.png';
-import pulseLogo from '@/assets/logo-pulse.png';
-import apexLogo from '@/assets/logo-apex.png';
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
 import Image from "next/image";
 
 export const LogoTicker = () => {
   return (
-    <div>
+    <div className="py-8 md:py-12 bg-white">
       <div className="container">
-        <div>
-          <Image src={acmeLogo} alt="Acme logo"/>
-          <Image src={quantumLogo} alt="Quantum logo"/>
-          <Image src={echoLogo} alt="Echo logo"/>
-          <Image src={celestialLogo} alt="Celestial logo"/>
-          <Image src={pulseLogo} alt="Pulse logo"/>
-          <Image src={apexLogo} alt="Apex logo"/>
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image src={acmeLogo} className="logo-ticker-img" alt="Acme logo" />
+            <Image
+              src={quantumLogo}
+              className="logo-ticker-img"
+              alt="Quantum logo"
+            />
+            <Image src={echoLogo} className="logo-ticker-img" alt="Echo logo" />
+            <Image
+              src={celestialLogo}
+              className="logo-ticker-img"
+              alt="Celestial logo"
+            />
+            <Image
+              src={pulseLogo}
+              className="logo-ticker-img"
+              alt="Pulse logo"
+            />
+            <Image src={apexLogo} className="logo-ticker-img" alt="Apex logo" />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
-
-
+  );
+};
