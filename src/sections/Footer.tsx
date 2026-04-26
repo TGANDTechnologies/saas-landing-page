bcca285ac55b33366655b6aea07663aae1744f9b
TGANDTechnologies <115176501+TGANDTechnologies@users.noreply.github.com>
2026-04-26 08:19:07 -0700

    logo swap

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
     <footer className="bg-black text-[#bcbcbc] text-sm py-10 text-center">
       <div className="container">
         <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:w-full before:bg-['linear-gradient(to_right,#f87bff, #fb92cf, #ffdd9b, #c2f0b1, #2fd8fe)'] before:absolute">
-          <Image src={logo} height={40} alt="SaaS logo" className="relative" />
+          <Image src={logo} height={40} alt="Solamailer logo" className="relative" />
         </div>
       </div>
       <nav className="flex flex-col gap-6 mt-6 md:flex-row md:justify-center">
